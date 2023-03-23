71fdc85ef0e5088b4b3a39935ab6db5feffcb3aa
nancy <nancyverma632002@gmail.com>
2023-03-23 11:59:45 +0530

    lazy loading

---
+++ 23_mar/routingLazyLoading/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },  //this tells the rote when home is entered go to homemodule(home component)
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
