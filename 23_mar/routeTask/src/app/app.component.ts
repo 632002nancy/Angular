e2346c36afd6bcc577817b4cbc70eafa4a93707b
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-03-24 18:20:30 +0530

    srcdest

--- 23_mar/routeTask/src/app/app.component.ts
+++ 23_mar/routeTask/src/app/app.component.ts
@@ -7,22 +7,26 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'routeTask';
-  @ViewChild('myform') form!: NgForm;  //! operator you basically say: I know what I'm doing and I know that angular will set this value for me
+  @ViewChild('myform') form!: NgForm;  //! form is a property which is saving this ngForm, operator you basically say: I know what I'm doing and I know that angular will set this value for me
 
   src:string='';
   dest:string='';
+
+  isDisable:boolean=true;
 
   cities:Array<string>=["Delhi", "Mumbai", "Banglore", "Kolkata"];
 
   
 
   printRoute(){
+    this.isDisable=false;
     console.log(this.form);  //console object of type ngform
     // console.log(this.cities[0]);
-   
     this.src=(this.form.value.source);
     console.log(this.src);
     this.dest=(this.form.value.destination);
     console.log(this.dest);
+
+    this.form.reset();  //set form control and form state to empty
   }
 }
