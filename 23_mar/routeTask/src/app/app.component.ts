386212d45df7e2b1d91960faf946ad191ea22b11
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-03-26 13:33:58 +0530

    a

--- 23_mar/routeTask/src/app/app.component.ts
+++ 23_mar/routeTask/src/app/app.component.ts
@@ -5,15 +5,20 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'routeTask';
   @ViewChild('myform') form!: NgForm;  //! form is a property which is saving this ngForm, operator you basically say: I know what I'm doing and I know that angular will set this value for me
+  @ViewChild('sourceSelect') srcOption!: Element;
+  @ViewChild('destSelect') destOption!: Element;
 
   src:string='';
   dest:string='';
   total_route:Array<string> = [];
   selectedSource:number=0;
   selectedDest:number=0;
+
+  obj:any={};
 
 
   isDisable:boolean=true;
@@ -26,14 +31,10 @@ export class AppComponent {
     console.log(this.selectedSource);
     for (let i = 0; i < this.cities.length; i++) {
       if(i===this.selectedSource){
-        console.log(this.form)
+        // this.destOption.options[this.selectedSource].disabled=true;
       }
       
-    }
-   
-    
-
-   
+    }   
       // console.log(item.selectedIndex);
     // this.form.value.destination.isDisable='true';
   }
@@ -52,6 +53,10 @@ export class AppComponent {
     // console.log(this.src);
     this.dest=(this.form.value.destination);
     // console.log(this.dest);
+    this.obj = { from: this.form.value.source, to: this.form.value.destination };           //creating array to store from and to data 
+    this.total_route.push(this.obj);
+    console.log(this.total_route);
+
 
     this.form.reset();  //set form control and form state to empty
   }
--- 23_mar/srcDest/src/app/app.component.ts
+++ 23_mar/srcDest/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'srcDest';
+  @ViewChild('sourceSelect') srcOption!: ElementRef;
+  @ViewChild('destSelect') destOption!: ElementRef;
+
 
   src:string='';
   dest:string='';
+  printSource:string='';
+  printDest:string='';
+  selectedSrc:number=0;
+  selectedDest:number=0;
+  obj:any={};
+  total_route:Array<string>=[];
 
   cities:Array<string>=["Delhi", "Mumbai", "Banglore", "Kolkata"];
+  
+  destdisable(item:string){
+    this.src=item;
+    console.log(this.src);
+    this.selectedSrc=this.cities.indexOf(item);
+    console.log(this.selectedSrc);
+    //disabling the current city in destination list
+    for (let i = 0; i < this.cities.length; i++) {
+      if(i===this.selectedSrc){
+        this.destOption.nativeElement.options[this.selectedSrc+1].disabled=true;
+        // console.log(this.destOption.nativeElement.options[this.selectedSrc+1])
+        continue;
+      }
+      this.destOption.nativeElement.options[this.selectedSrc].disabled=false;
+    }
 
-  printRoute(){
-   
+    //disabling all destinations with same source (item)
+    for (let i = 0; i < this.cities.length; i++) {
+      console.log(this.total_route[i])
   }
+  }
+  sorcedisable(item:string){
+    this.dest=item;
+    console.log(this.dest);
+    this.selectedDest=this.cities.indexOf(item);
+    console.log(this.selectedDest);
+    for (let i = 0; i < this.cities.length; i++) {
+      // console.log(i)
+      if(i===this.selectedSrc){
+        // console.log("yes")
+      }
+      
+    }
+  }
+  printRoute(source:string, destination:string){
+   this.printSource=source;
+   this.printDest=destination;
 
+   this.obj = { from:source , to:destination };           //creating array to store from and to data 
+    this.total_route.push(this.obj);
+    console.log(this.total_route);
+  }
 }
