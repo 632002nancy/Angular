f0f6a69488ca95eb031bc7f8eaea4e586664916e
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-03-28 11:47:18 +0530

    completed srcDest using ngModel

--- 27_mar/matSelect/src/app/app.component.ts
+++ 27_mar/matSelect/src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component,ViewChild } from '@angular/core';
 import { NgModel } from "@angular/forms";
-import { MatSelect } from '@angular/material/select';
+interface cities{
+  name:string;
+  disabled:boolean;
+}
+
+interface routes{
+  from:string;
+  to:string;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,30 +18,85 @@ import { MatSelect } from '@angular/material/select';
 export class AppComponent {
   title = 'matSelect';
 
-  constructor(){
-    console.log("constructor invoked")
-  }
-
   source:string='';
   destination:string='';
-  selectedSrc:number=0;
-  selectedDest:number=0;
+
+  obj:routes={from:'',to:''};
   
-  cities:Array<string>=["","Delhi", "Mumbai", "Banglore", "Kolkata", "Sikkim"];
+  sourceList: cities[] = [
+    { name: '', disabled: true},
+    { name: 'Delhi', disabled: false},
+    { name: 'Kolkata', disabled: false},
+    { name: 'Mumbai', disabled: false},
+    { name: 'Jaipur', disabled: false},
+    { name: 'Pune', disabled: false},
+  ];
+  destinationList: cities[] = [
+    { name: '', disabled: true},
+    { name: 'Delhi', disabled: false},
+    { name: 'Kolkata', disabled: false},
+    { name: 'Mumbai', disabled: false},
+    { name: 'Jaipur', disabled: false},
+    { name: 'Pune', disabled: false},
+  ];
+
+  totalRoutes:Array<routes>=[];
 
   disableDest() { 
-    console.log("select event emitted when source changed");
     console.log(this.source);
-    this.selectedSrc=this.cities.indexOf(this.source);
-    console.log(this.selectedSrc);
+    //disabling the current city in destination drop-down
+    for (let i = 0; i < this.destinationList.length; i++) {
+      if(this.destinationList[i].name===this.source){
+        this.destinationList[i].disabled=true;
+      }     
+    }
+
+    for (let i = 0; i < this.totalRoutes.length; i++) {
+      if(this.totalRoutes[i].from===this.source){
+          for (let j = 0; j < this.destinationList.length; j++) {
+            if(this.destinationList[j].name===this.totalRoutes[i].to){
+              this.destinationList[j].disabled=true;
+            }
+          }
+        }
+    }
   }
 
   disableSrc() { 
-    console.log("select event emitted when destination changed");
-    console.log(this.destination)
-    this.selectedDest=this.cities.indexOf(this.destination);
-    console.log(this.selectedDest);
+    console.log(this.destination);
+     //disabling the current city in source drop-down
+    for (let i = 0; i < this.destinationList.length; i++) {
+      if(this.sourceList[i].name===this.destination){
+        this.sourceList[i].disabled=true;
+      }     
+    }
+
+    for (let i = 0; i < this.totalRoutes.length; i++) {
+      if(this.totalRoutes[i].to===this.destination){
+          for (let j = 0; j < this.sourceList.length; j++) {
+            if(this.sourceList[j].name===this.totalRoutes[i].from){
+              this.sourceList[j].disabled=true;
+            }
+          }
+        }
+    }
   }
 
+  addRoute(){
+    console.log(this.totalRoutes)
+    this.obj={from:this.source, to:this.destination}
+    this.totalRoutes.push(this.obj);   //adding routes in the totalRoutes array
+    console.log(this.totalRoutes);
+
+    for(let i=0;i<this.sourceList.length;i++){              //enable all the options in both lists when printing is done
+      this.sourceList[i].disabled=false;
+      this.destinationList[i].disabled=false;
+    }
+
+  }
+
+  clearRoute(){
+    location.reload();
+  }
 
 }
