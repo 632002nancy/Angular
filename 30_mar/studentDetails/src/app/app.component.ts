ed3ab3ff2551e9046f01cc6c206fdeebb9d8e0da
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-03-30 13:43:55 +0530

    studentDetails with sub button

--- 30_mar/studentDetails/src/app/app.component.ts
+++ 30_mar/studentDetails/src/app/app.component.ts
@@ -25,17 +25,18 @@ export class AppComponent implements OnInit {
     });
   }
 
-    addSubject(){
+    addSubject():void{
       (<FormArray>this.reactiveForm.get('subjects')).push(new FormControl(null,Validators.required)) //this this.reactiveForm.get('subjects') gives value of type formControl formArray and more so we need to explicity typecast this as a type of formArray
     }  //here we are pushing new formArray inside the formArray(subjects), so we need to loop over that array in html
 
-    onSubmit(){
+    onSubmit():void{
       console.log("Details Of the student are:")
       console.log(this.reactiveForm.value.fullName);
       console.log(this.reactiveForm.value.age);
       console.log(this.reactiveForm.value.email);
       console.log(this.reactiveForm.value.stream);
       console.log(this.reactiveForm.value.subjects);
+      console.log(this.reactiveForm.get('subjects')['controls']);
     }
   }
   
