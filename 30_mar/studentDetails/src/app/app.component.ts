b01ca293d632bdcbf2472e52dea1e499ead2fda8
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-03-30 18:43:20 +0530

    del sub btn for every sub

--- 30_mar/studentDetails/src/app/app.component.ts
+++ 30_mar/studentDetails/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup, FormArray} from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
+import { findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,40 +9,45 @@ import { FormControl, Validators, FormGroup, FormArray} from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'studentDetails';
-  
+
   streams: string[] = ["CSE", "ME", "ECE", "EE", "CE"];
-  showConfirmation:boolean=false;
+  showConfirmation: boolean = false;
 
   reactiveForm: FormGroup;
 
-  ngOnInit(){
-    this.reactiveForm=new FormGroup({  //here form controls are created in key value pairs
-      fullName:new FormControl(null,Validators.required),
-      age:new FormControl(null,[Validators.required,Validators.min(15),Validators.max(20)]),
-      email:new FormControl(null,[Validators.required,Validators.email]),
-      stream:new FormControl(null,[Validators.required]),
-      subjects:new FormArray([   //here form controls are created in the form of an array
-        new FormControl(null,Validators.required),
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({  //here form controls are created in key value pairs
+      fullName: new FormControl(null, Validators.required),
+      age: new FormControl(null, [Validators.required, Validators.min(15), Validators.max(20)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      stream: new FormControl(null, [Validators.required]),
+      subjects: new FormArray([   //here form controls are created in the form of an array
+        new FormControl(null, Validators.required),
       ])
     });
   }
 
-    addSubject():void{
-      (<FormArray>this.reactiveForm.get('subjects')).push(new FormControl(null,Validators.required)); //this this.reactiveForm.get('subjects') gives value of type formControl formArray and more so we need to explicity typecast this as a type of formArray
-    }  //here we are pushing new formArray inside the formArray(subjects), so we need to loop over that array in html
+  addSubject(): void {
+    (<FormArray>this.reactiveForm.get('subjects')).push(new FormControl(null, Validators.required)); //this this.reactiveForm.get('subjects') gives value of type formControl formArray and more so we need to explicity typecast this as a type of formArray
+  }  //here we are pushing new formArray inside the formArray(subjects), so we need to loop over that array in html
 
-    deleteSubjectMssg(i:number){
-      this.showConfirmation=true;
-    }
-    hideMssg(){
-      this.showConfirmation=false;
-    }
-    deleteSubject(delSubIndex:number):void{
-      (<FormArray>this.reactiveForm.get('subjects')).removeAt(delSubIndex);
-    } 
-
-    onSubmit():void{
-      console.log(this.reactiveForm)
-      this.reactiveForm.reset();
-    }
+  deleteSubjectMssg(i: number) {
+    // console.log( this.reactiveForm.value.subjects.length);
+    // console.log( this.reactiveForm.get('subjects').value);
+    // for (let i = 0; i < this.reactiveForm.value.subjects.length; i++) {
+    //   if(findIndex(this.reactiveForm.value)===i)
+    // }
+    this.showConfirmation = true;
   }
+  hideMssg() {
+    this.showConfirmation = false;
+  }
+  deleteSubject(delSubIndex: number): void {
+    (<FormArray>this.reactiveForm.get('subjects')).removeAt(delSubIndex);
+  }
+
+  onSubmit(): void {
+    console.log(this.reactiveForm)
+    this.reactiveForm.reset();
+  }
+}
