e42c2c131f76c6c9984176b9b6278c6128f6665c
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-04-03 15:03:50 +0530

    complete

--- April/3_apr/webAppLazy/src/app/app-routing.module.ts
+++ April/3_apr/webAppLazy/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'services', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) }, { path: 'goals', loadChildren: () => import('./goals/goals.module').then(m => m.GoalsModule) }, { path: 'aboutUs', loadChildren: () => import('./about-us/about-us.module').then(m => m.AboutUsModule) }];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },   //go and find HomeModule and display default file for homeModule
+  { path: 'services', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) },
+  { path: 'goals', loadChildren: () => import('./goals/goals.module').then(m => m.GoalsModule) }, 
+  { path: 'aboutUs', loadChildren: () => import('./about-us/about-us.module').then(m => m.AboutUsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
