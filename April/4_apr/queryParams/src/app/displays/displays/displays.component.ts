51955707141f4e34e07f84528ab146ab8229e804
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-04-05 18:20:40 +0530

    query params

--- April/4_apr/queryParams/src/app/displays/displays/displays.component.ts
+++ April/4_apr/queryParams/src/app/displays/displays/displays.component.ts
@@ -12,6 +12,7 @@ export class DisplaysComponent {
   queryPage=0;
   constructor(private activatedRoute:ActivatedRoute){
     activatedRoute.queryParams.subscribe(data=>{    //we read the data that is passed from subscribe call
+      console.log(data)
         this.queryCountry=data['country'];  //retrieving the data of query params
         this.queryTrending=data['trending'];
         this.queryPage=data['page'];
