e46c35ff5d5853ce14032f2f6ea15f6ebcfe9aae
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-04-14 13:16:11 +0530

    a

--- April/14_mar/multiUser/src/app/admin/admin-routing.module.ts
+++ April/14_mar/multiUser/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = 
+[
+  { path: '', component: AdminComponent },
+  {path:'admin/products', component:ProductsComponent},
+  {path:'admin/customers', component:CustomersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- April/14_mar/multiUser/src/app/users/users-routing.module.ts
+++ April/14_mar/multiUser/src/app/users/users-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = 
+[
+  { path: '', component: UsersComponent },
+  { path: 'users/facilities', component: FacilitiesComponent },
+  { path: 'users/products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
