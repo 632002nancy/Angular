00868d6639304982fd00b3acf6c0abe0224a21f3
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-04-14 13:55:37 +0530

    a

--- April/14_mar/multiUser/src/app/welcome/welcome.component.ts
+++ April/14_mar/multiUser/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,13 +8,11 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  constructor(private router:Router){}
   adminClicked:boolean=false;
   userClicked:boolean=false;
   loginAdmin:string="admin";
   loginAdminPass:string="1234";
- 
- @ViewChild('adminForm') aform:NgForm;
- @ViewChild('userForm') uform:NgForm;
  
    showAdmin(){
      this.adminClicked=true
@@ -25,10 +24,12 @@ export class WelcomeComponent {
    adminVerified(data){
      if(this.loginAdmin===data.adminName && this.loginAdminPass===data.adminPass){
        console.log("verified");
-     }
+      }
+      this.router.navigate(['/admin']);
      
    }
    userVerified(data:string){
      console.log(data)
+     this.router.navigate(['/users']);
    }
 }
