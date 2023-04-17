2641b34e048ca207fd21e80689bf2cd1c693b883
Nancy Verma <124571076+632002nancy@users.noreply.github.com>
2023-04-17 12:16:04 +0530

    a

--- April/14_mar/nodeJWT/index.js
+++ April/14_mar/nodeJWT/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
+const secreteKey = "secretKey";
 
 app.get("/", (req, res) => {
   res.json({
@@ -15,8 +16,39 @@ app.post("/", (req, res) => {
     email: "nancy@gmail.com",
     password: "1234"
   }
-  jwt.sign
+  jwt.sign({ user }, secreteKey, { expiresIn: '500s' }, (err, token) => {//details(here userDetails , secret key , expiration time)
+    res.json({ user, token });
+  })
 })
+
+app.post("/profile", verify, (req, res) => {
+  jwt.verify(req.token, secreteKey, (err, authData) => {
+    if (err) {
+      res.send({ result: "Invalid token" })
+    } else {
+      res.json({
+        messgae: "Profile accessed",
+        authData
+      })
+    }
+  })
+})
+
+//getting he token
+function verify(req, res, next) {
+  const bearerHeader = req.headers.Authorization || req.headers.authorization;
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    req.token = token;
+    next();
+  }
+  else {
+    res.send({
+      result: 'Token is not valid'
+    })
+  }
+}
 
 app.listen(5000, () => {
   console.log("running on 5000")
